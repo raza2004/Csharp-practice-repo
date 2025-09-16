a0e1b8c34a2a40126e0bc4b5f2946b7108734e83
Owais Raza <owaisraza3274@gmail.com>
2025-09-16 23:13:28 +0500

    9/16/2025

--- Program.cs
+++ Program.cs
@@ -1,5 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
 using System.Drawing;
 using Pastel;
 
-Console.WriteLine("Hello, Owais!" .Pastel(Color.Aqua));
+// Console.WriteLine("Hello, Owais!".Pastel(Color.Aqua));
+
+
+// A method can use memebers of class but function cannot use members of class
+
+//Calculator class
+public class Calculator
+{
+    public static int number1;
+    public static int number2;
+
+    public static int Add()
+
+    {
+        return number1 + number2;
+    }
+ public static void DisplayNumbers()
+     {
+       for (int a = 0; a <= 20; a++)
+        {
+            Console.WriteLine(a);         }
+    }
+    static void Main(string[] args)
+    {
+        number1 = 34;
+        number2 = 45;
+        int result = Add();
+        DisplayNumbers();
+    
+        Console.WriteLine("The result is " + result);
+    }
+
+}
+
+// For loop
+
+// public class NumberDisplay()
+// {
+
+//     public static void DisplayNumbers()
+//     {
+//         for (int a = 0; a <= 20; a++)
+//         {
+//             Console.WriteLine(a);
+//         }
+//     }
+//     static void Main(string[] args)
+//     {
+//         DisplayNumbers();
+//     }
+
+// }
+
+
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98eb7fcbb4429956b12dd175e08c59fa02b766b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
